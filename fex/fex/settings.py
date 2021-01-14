dbb216dd29669e6ce7e086ab59880fc1126a82b9
Pavel Popov <popovpp@yandex.ru>
2021-01-14 03:13:05 +0300

    create new user model

--- fex/fex/settings.py
+++ fex/fex/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fex_account',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'fex_account.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
--- fex/fex_account/models.py
+++ fex/fex_account/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+#from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import CustomUserManager
+
+from django.conf import settings
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    balance = models.IntegerField()
+    freeze_balanca = models.IntegerField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email

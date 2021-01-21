41f1ab704ca021ce529815be5b44a161918d5cef
Pavel Popov <popovpp@yandex.ru>
2021-01-21 09:51:02 +0300

    add token-auth, made get_permission

--- fex/account/views.py
+++ fex/account/views.py
@@ -14,27 +14,33 @@ class UserViewSet(viewsets.ModelViewSet):
 	"""
 	API endpoint that allows users to be viewed or edited.
 	"""	
-	permission_classes = [IsOwnerOnly | permissions.IsAdminUser]#, permissions.IsAuthenticated]
+	#permission_classes = [IsOwnerOnly | permissions.IsAdminUser]#, permissions.IsAuthenticated]
 	#queryset = User.objects.all().order_by('-date_joined')
 	serializer_class = UserSerializer
 
 	def get_queryset(self):
-		if self.request.user.is_superuser:
-			queryset = User.objects.all().order_by('-date_joined')
-		elif not self.request.user.is_authenticated:
-			queryset = []
-		else:
+
+		if self.request.user.is_authenticated and not self.request.user.is_superuser:
 			queryset = User.objects.filter(email=self.request.user.email)
+		else:
+			queryset = User.objects.all().order_by('-date_joined')
+#		
+#		if self.request.user.is_superuser:
+#			queryset = User.objects.all().order_by('-date_joined')
+#		elif not self.request.user.is_authenticated:
+#			queryset = []
+#		else:
+#			queryset = User.objects.filter(email=self.request.user.email)
 		return queryset
 	
-#	def get_permissions(self):
-#		#is_superuser = self.request.user.is_superuser
-#		"""
-#    	Instantiates and returns the list of permissions that this view requires.
-#    	"""
-#        
-#
-#		if self.action == 'list':
+	def get_permissions(self):
+
+		if self.request.method == 'GET':
+			self.permission_classes = [permissions.IsAuthenticated & (IsOwnerOnly | permissions.IsAdminUser), ]
+		else:
+			self.permission_classes = [IsOwnerOnly | permissions.IsAdminUser, ]
+		return super(UserViewSet, self).get_permissions()
+        #		if self.action == 'list':
 #			permission_classes = [permissions.IsAdminUser]
 #		else:
 #			permission_classes = [IsOwnerOnly | permissions.IsAdminUser, permissions.IsAuthenticated]
